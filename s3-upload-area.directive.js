214b098894d1221950925052a8e8b6e3c85c5cbd
Koen Vendrik <k.vendrik@gmail.com>
2015-10-11 16:04:00 +0200

    Adds option to edit messages, adds attr to only allow certain filetypes

--- s3-upload-area.directive.js
+++ s3-upload-area.directive.js
@@ -8,19 +8,70 @@ angular
             scope: {
                 creds: '=',
                 errorEmail: '@',
-                errorSubject: '@'
+                errorSubject: '@',
+                onSuccess: '&',
+                messages: '=',
+                allowedFileTypes: '@'
             },
             templateUrl: 'templates/s3-upload-area.directive.html',
 
             link: function(scope, element, attrs){
+
+                var msgHelper = {
+                    _messages: {
+                        default: {
+                            title: 'Upload File',
+                            desc: 'Click here to upload'
+                        },
+                        noFile: {
+                            title: 'No file selected',
+                            desc: 'Click here to upload'
+                        },
+                        success: {
+                            title: 'Done uploading %filename%',
+                            desc: 'Click here to upload another'
+                        },
+                        error: {
+                            title: 'Whoops Error!',
+                            desc: '<a href="mailto:%email%?subject=%subject%&body=%msg%">Let us know about it</a>'
+                        },
+                        uploading: {
+                            title: 'Uploading...',
+                            desc: '%filename%'
+                        },
+                        incorrectFileType: {
+                            title: 'Whoops! That file type is not supported',
+                            desc: 'The supported filestypes are %allowedFileTypes%'
+                        }  
+                    },
+
+                    init: function(){
+                        if(typeof scope.messages === 'object'){
+                            angular.extend(this._messages, scope.messages);
+                        }
+                    },
+
+                    _parseVarsInStr: function(str, vars){
+                        for(var key in vars){
+                            str = str.replace('%'+key+'%', vars[key]);
+                        }
+                        return str;
+                    },
+
+                    get: function(state, field, vars){
+                        return this._parseVarsInStr(this._messages[state][field], vars);
+                    }
+                };
+
+                msgHelper.init();
 
                 var s3upload = new S3Upload({
                     creds: scope.creds,
                     onStart: function(file){
                         scope.$apply(function(){
                             scope.helper.text = {
-                                title: 'Uploading...',
-                                desc: file.name
+                                title: msgHelper.get('uploading', 'title', {}),
+                                desc: msgHelper.get('uploading', 'desc', { filename: file.name })
                             };
                             scope.helper.uploading = true;
                         });
@@ -28,8 +79,12 @@ angular
                     onError: function(err, file){
                         scope.$apply(function(){
                             scope.helper.text = {
-                                title: 'Whoops Error!',
-                                desc: '<a href="mailto:'+scope.errorEmail+'?subject='+scope.errorSubject+'&body='+err.message+'">Let us know about it</a>'
+                                title: msgHelper.get('error', 'title', {}),
+                                desc: msgHelper.get('error', 'desc', {
+                                    email: scope.errorEmail,
+                                    subject: scope.errorSubject,
+                                    msg: err.message
+                                })
                             };
                             scope.helper.uploading = false;
                             scope.helper.uploadProgress = 0;
@@ -38,11 +93,12 @@ angular
                     onSuccess: function(url, file){
                         scope.$apply(function(){
                             scope.helper.text = {
-                                title: 'Done uploading '+file.name,
-                                desc: 'Click here to upload another video'
+                                title: msgHelper.get('success', 'title', { filename: file.name }),
+                                desc: msgHelper.get('success', 'desc', {})
                             };
                             scope.helper.uploading = false;
                             scope.helper.uploadProgress = 0;
+                            scope.onSuccess({ url: url, file: file });
                         });
                     },
                     onProgress: function(percentage){
@@ -52,14 +108,28 @@ angular
                     }
                 });
 
+                var allowedFileTypes = scope.allowedFileTypes.split(',');
                 var uploadFile = function(file){
+                    if(allowedFileTypes.indexOf(file.type) === -1){
+                        //unsupported filetype
+                        scope.$apply(function(){
+                            scope.helper.text = {
+                                title: msgHelper.get('incorrectFileType', 'title', {}),
+                                desc: msgHelper.get('incorrectFileType', 'desc', {
+                                    allowedFileTypes: allowedFileTypes.join(', ')
+                                })
+                            };
+                        });
+                        return;
+                    }
+
                     if(file){
                         s3upload.uploadFile(file);
                     } else {
                         // No File Selected
                         scope.helper.text = {
-                            title: 'No file selected',
-                            desc: 'Click here and upload a .mp4 file'
+                            title: msgHelper.get('noFile', 'title', {}),
+                            desc: msgHelper.get('noFile', 'desc', {})
                         };
                     }
                 };
@@ -73,8 +143,8 @@ angular
 
                 scope.helper = {
                     text:{
-                        title: 'Upload Video File',
-                        desc: 'Click here and upload a .mp4 file'
+                        title: msgHelper.get('default', 'title', {}),
+                        desc: msgHelper.get('default', 'desc', {})
                     },
                     uploading: false,
                     uploadProgress: 0,
